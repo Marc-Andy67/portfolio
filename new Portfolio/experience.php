89e3e2267c50f9648e947bb505f75c4c6078d6a5
Marc-Andy67 <144818191+Marc-Andy67@users.noreply.github.com>
2024-03-10 21:42:32 +0100

    Add files via upload

---
+++ new Portfolio/experience.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Mon portefolio</title>
+</head>
+
+<?php include('header.php') ?>
+
+<body>
+
+    <section class="page-content">
+        <header>
+            <h1>Expériences Professionnelles</h1>
+        </header>
+
+        <section class="experience-section">
+            <img class="experience-image" src="https://i.ibb.co/Hrn34bf/201603-mooc-fabnum-964x1030-e0838.jpg" alt="Image de présentation" />
+            <p class="experience-description">
+                J'ai réalisé un stage d'observation à Euroinformation pour m'approprier les connaissances du milieu informatique et le fonctionnement de Euroinformation.
+            </p>
+            <p class="experience-description">
+                J'ai ensuite réalisé un stage professionnelisant dans l'école privée Stralang où j'ai été chargé d'améliorer l'interface visuelle du site web avec l'aide du CMS WordPress. J'ai également été chargé de manipuler et de maintenir le domaine informatique de l'organisation avec des solutions réseau.
+            </p>
+        </section>
+
+    </section>
+
+</body>
+
+<?php include('footer.php') ?>
+
+</html>
---
+++ new Portfolio/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Mon portefolio</title>
+</head>
+<?php include('header.php') ?>
+<body>
+<section class="presentation-image">
+    <img class="presentation-image" src="https://i.ibb.co/RbGysWw/main-image.jpg" alt="AI image" />
+</section>
+
+<section class="projects-section">
+    <h1>Réalisations</h1>
+    <div class="projects">
+        <div class="project">
+            <h4>Un site portefolio</h4>
+            <p class="description">
+                J'ai réalisé un site portfolio à l'aide de mes compétences acquises pour répertorier mes compétences.
+            </p>
+            <p class="dates">2022</p>
+        </div>
+
+        <div class="project">
+            <h4>Un répertoire des métiers de l'informatique</h4>
+            <p class="description">
+                Nous avons répertorié les métiers de l'informatique sur une carte mentale afin de pouvoir se repérer à la fin de cette formation.
+            </p>
+            <p class="dates">2022</p>
+        </div>
+
+        <div class="project">
+            <h4>Mise en situation professionnelle</h4>
+            <p class="description">
+                J'ai reçu des missions similaires à des possibles missions dans un milieu professionnel pour me préparer.
+            </p>
+            <p class="dates">2022</p>
+        </div>
+
+        <div class="project">
+            <h4>Stage professionnelisant</h4>
+            <p class="description">
+                J'ai réalisé un stage de 6 semaines durant ma formation pour valider mon année scolaire et pouvoir mettre mes compétences au service de mon organisme. Le stage s'est déroulé dans une école privée où j'ai été chargé de l'amélioration de l'interface web de cette école et de la maintenance du patrimoine informatique.
+            </p>
+            <p class="dates">2023</p>
+        </div>
+
+        <div class="project">
+            <h4>Appli mobile de gestion de risque</h4>
+            <p class="description">
+                J'ai réalisé une application mobile sous WinDev durant mes études permettant de calculer le niveau de risque du patrimoine informatique d'une entreprise en prenant en compte les conseils et mesures à prendre pour sécuriser son patrimoine, et en calculant le risque individuel de chaque ressource informatique.
+            </p>
+            <p class="dates">2023</p>
+        </div>
+    </div>
+</section>
+
+</body>
+<?php include('footer.php') ?>
+</html>
